336a983d41279897bdfdc23276f64ef7ed9be008
Indradb <60851042+Indra-db@users.noreply.github.com>
2023-08-16 21:47:19 +0100

    setup sdl2 basic window

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,61 @@
 #![allow(dead_code)]
+#![allow(unused_imports)]
 
+extern crate sdl2;
 mod math;
 
-use math::evector::FVector4;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use std::time::Duration;
 
-fn main() {}
+fn print_key_mapping() {
+    println!(
+        "\n\n Raytracing in rust \n\n\n\
+              Moving: WASD\n\
+              Rotating: Hold Left Mouse Button\n\n\n\
+              ---- The following keys correspond to the corresponding actions ----\n\n\
+              Z: Turn off shadow casting \n\
+              X: Screenshot (saved in solution folder) \n\
+              C: Change the amount of bounces (1,2,3,4 bounces) \n\
+              V: Change Render modes (irradiance only, BRDF only and all) \n\n\
+              P: Cycle through the lights in the scene to choose which one to select \n\
+              O: Switch between 'Change Color' or 'Change Position' for the selected light \n\
+              Y: Go to previous scene\n\
+              U: Go to next scene\n\
+              I: Cycle through the cube maps loaded in \n\
+              0: Turn off selected light \n\n\
+              1 & 2: Change x/r value of the selected light pos/color \n\
+              3 & 4: Change y/g value of the selected light pos/color \n\
+              5 & 6: Change z/b value of the selected light pos/color \n\
+              7 & 8: Change intensity of the selected light \n\n\
+              M: Print key mapping again \n\
+              N: Print FPS \n\n"
+    );
+}
+
+fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem.window("SDL2 Demo", 800, 600).position_centered().build().unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.set_draw_color(Color::RGB(255, 0, 0));
+    canvas.clear();
+    canvas.present();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                }
+                _ => {}
+            }
+        }
+    }
+}
